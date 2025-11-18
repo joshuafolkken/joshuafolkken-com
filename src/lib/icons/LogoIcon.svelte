e9f903b0b807c3a6178294d076a4928cefa9fee1
joshuafolkken <joshuafolkken@gmail.com>
2025-11-18 14:36:30 +0900

    "favicon #77"

--- src/lib/icons/LogoIcon.svelte
+++ src/lib/icons/LogoIcon.svelte
@@ -11,20 +11,31 @@
 
 <a href={resolve('/')}>
 	<svg
+		xmlns="http://www.w3.org/2000/svg"
 		viewBox="20 20 45 55"
 		width={size}
 		height={size}
-		class=" text-white"
+		class="text-white"
 		aria-label="Geometric JF Fusion Logo"
 	>
+		<defs>
+			<filter id="outline">
+				<feMorphology operator="dilate" radius="2" in="SourceAlpha" result="dilated"></feMorphology>
+				<feFlood flood-color="black" flood-opacity="1" result="flood"></feFlood>
+				<feComposite in="flood" in2="dilated" operator="in" result="outline"></feComposite>
+				<feComposite in="SourceGraphic" in2="outline" operator="over"></feComposite>
+			</filter>
+		</defs>
 		<title>Geometric JF Fusion</title>
-		<!-- J の要素 -->
-		<rect x="20" y="20" width="8" height="50" fill="currentColor"></rect>
-		<rect x="20" y="20" width="20" height="8" fill="currentColor"></rect>
-		<rect x="20" y="62" width="12" height="8" fill="currentColor"></rect>
-		<!-- F の要素（J と重なる） -->
-		<rect x="40" y="20" width="8" height="55" fill="currentColor"></rect>
-		<rect x="40" y="20" width="25" height="8" fill="currentColor"></rect>
-		<rect x="40" y="45" width="20" height="8" fill="currentColor"></rect>
+		<g filter="url(#outline)">
+			<!-- J の要素 -->
+			<rect x="20" y="20" width="8" height="50" fill="currentColor"></rect>
+			<rect x="20" y="20" width="20" height="8" fill="currentColor"></rect>
+			<rect x="20" y="62" width="12" height="8" fill="currentColor"></rect>
+			<!-- F の要素（J と重なる） -->
+			<rect x="40" y="20" width="8" height="55" fill="currentColor"></rect>
+			<rect x="40" y="20" width="25" height="8" fill="currentColor"></rect>
+			<rect x="40" y="45" width="20" height="8" fill="currentColor"></rect>
+		</g>
 	</svg>
 </a>
