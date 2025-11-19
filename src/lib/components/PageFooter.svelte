b14ed6107adbec93db4f86447de932bdb7ddefc8
joshuafolkken <joshuafolkken@gmail.com>
2025-11-19 14:22:39 +0900

    "display donations in the footer #116"

--- src/lib/components/PageFooter.svelte
+++ src/lib/components/PageFooter.svelte
@@ -2,8 +2,13 @@
 	import { resolve } from '$app/paths'
 	import { APP, AUTHOR } from '$lib/app'
 	import { PAGES } from '$lib/types/page'
+	import Divider from './Divider.svelte'
+	import LinkCard from './LinkCard.svelte'
 </script>
 
+<LinkCard page={PAGES.DONATIONS} />
+
+<Divider />
 <footer class="mt space-y-1 text-center text-sm text-white/60">
 	{#if PAGES.PRIVACY_POLICY.link}
 		<a href={resolve(PAGES.PRIVACY_POLICY.link as '/privacy-policy')} class="link-base">
