f107713f69c505710bcd43c1ae485b9e0c3cf5d5
joshuafolkken <joshuafolkken@gmail.com>
2025-11-19 13:29:28 +0900

    "Google AdSense #102"

---
+++ src/lib/components/AdSenseScript.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1014823123178423"
+		crossorigin="anonymous"
+	></script>
+</svelte:head>
