3ef854fe910809096eef2e305bae08c03b9fcbde
Joshua Folkken <joshuafolkken@gmail.com>
2025-11-26 00:48:10 +0900

    "share buttons #137"

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
-		timeout: process.env.CI ? 15_000 : 10_000,
+		timeout: process.env.CI ? 15_000 : 25_000,
 	},
 	testDir: 'e2e',
 	fullyParallel: true,
