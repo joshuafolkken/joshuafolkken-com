484f26edecd753c1f96d6a82c3567f0cb587b148
joshuafolkken <joshuafolkken@gmail.com>
2025-11-28 23:27:29 +0900

    "update copy button #190"

--- src/lib/components/CopyButton.svelte
+++ src/lib/components/CopyButton.svelte
@@ -23,43 +23,45 @@
 	class="group relative inline-flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full border border-white/10 text-neutral-400 transition-all duration-300 hover:border-white/0 hover:bg-white/20 hover:text-white {class_name}"
 	aria-label="Copy Link"
 >
-	{#if is_copied}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="h-5 w-5"
-		>
-			<polyline points="20 6 9 17 4 12"></polyline>
-		</svg>
-	{:else}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			class="h-5 w-5"
-		>
-			<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-			<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-		</svg>
-	{/if}
-
-	<!-- Tooltip -->
-	<span
-		class="absolute -top-10 left-1/2 -translate-x-1/2 rounded bg-black px-2 py-1 text-xs whitespace-nowrap text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-	>
-		{is_copied ? 'Copied!' : 'Copy Link'}
-	</span>
+	<div class="relative">
+		{#if is_copied}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="h-5 w-5"
+			>
+				<polyline points="20 6 9 17 4 12"></polyline>
+			</svg>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="h-5 w-5"
+			>
+				<rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+				<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+			</svg>
+		{/if}
+		{#if is_copied}
+			<span
+				class="absolute -top-8 left-1/2 -translate-x-1/2 animate-bounce text-sm font-bold whitespace-nowrap text-white"
+			>
+				Copied!
+			</span>
+		{/if}
+	</div>
 </button>
