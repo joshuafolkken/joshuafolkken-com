ea68bc7d6de56f02b06bb8de7e2c843d52a4647f
joshuafolkken <joshuafolkken@gmail.com>
2026-06-03 14:32:29 +0700

    Remove orphaned local eslint/rules vendored config #616

--- eslint/rules/promise.ts
+++
@@ -1,7 +0,0 @@
-export const promise_rules = {
-	// Promise コンストラクタの適切な使用
-	'promise/no-new-statics': 'error',
-	'promise/no-return-wrap': 'error',
-	'promise/param-names': 'error',
-	'promise/no-nesting': 'error',
-}
